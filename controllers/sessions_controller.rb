8ba9b41299d9f8b55b79eb98117c556222904b25
jasongriggs <jay.griggs86@gmail.com>
2022-09-14 12:19:47 +0930

    CRUD Complete Signup Error

---
+++ controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+get '/sessions/new' do
+    erb :'sessions/new_session'
+  end
+
+post '/sessions/new_session' do
+    username = params['username']
+    password = params['password']
+  
+    user = find_user_by_username(username)
+  
+    if user && BCrypt::Password.new(user['password_digest']) == password
+      # log the user in if password matches
+      session['user_id'] = user['id']
+  
+      redirect '/'
+    end
+end
--- models/user.rb
+++ models/user.rb
@@ -1,3 +1,20 @@
 def create_user(username, password_digest)
-    run_sql("INSERT INTO users(username, password_digest ) VALUES($1, $2)", [username, password_digest])
+    password_secure = BCrypt::Password.create(password_digest)
+
+    run_sql("INSERT INTO users(username, password_digest ) VALUES($1, $2)", [username, password_secure])
+end
+
+def find_user_by_username(username)
+    users = run_sql("SELECT * FROM users WHERE username = $1", [username])
+    
+    # if more than one user, pick the first one, but if no one is found, return nil
+    if users.to_a.count > 0
+      users[0]
+    else
+      nil
+    end
+end
+
+def find_user_by_id(id)
+    run_sql("SELECT * FROM users WHERE id = $1", [id])[0]
 end
