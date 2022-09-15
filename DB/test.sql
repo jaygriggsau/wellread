71cc0a811d65adf148299060c9be52fe03ed0332
jasongriggs <jay.griggs86@gmail.com>
2022-09-15 10:06:10 +0930

    More CSS fixes

--- DB/test.sql
+++ DB/test.sql
@@ -2,3 +2,15 @@ SELECT stats.*, books.title, books.author, books.image_url FROM books
 INNER JOIN stats
 ON stats.books_id = books.id
 ORDER BY id DESC;
+
+SELECT stats.*, books.title, books.author, books.image_url FROM books
+INNER JOIN stats
+ON stats.books_id = books.id
+ORDER BY id DESC;
+
+FROM mytable
+INNER JOIN another_table 
+    ON mytable.id = another_table.id
+WHERE condition(s)
+ORDER BY column, … ASC/DESC
+LIMIT num_limit OFFSET num_offset;
